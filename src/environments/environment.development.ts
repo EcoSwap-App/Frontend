90cfb3594e5c3671c1b7ba6bcc5d3f31ebbd6a40
Lynn Meza <lynn770450@gmail.com>
2026-06-03 13:34:51 -0500

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderUsersEndpointPath: '/users',
+  platformProviderUniversitiesEndpointPath: '/universities',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderProductsEndpointPath: '/products',
+  platformProviderLocationsEndpointPath: '/locations',
+  platformProviderMessagesEndpointPath: '/messages',
+  platformProviderMeetingsEndpointPath: '/meetings',
+  platformProviderReputationsEndpointPath: '/reputations',
+  platformProviderReportsEndpointPath: '/reports',
+};
