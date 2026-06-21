22b62da8eb114591658dfea86ddc20458d8da853
jorgeLinares <jorgelinaresrrs306@hotmail.com>
2026-06-21 00:31:44 -0500

    feat(auth): protect application routes using a functional auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,29 +4,28 @@ import { Register } from './pages/register/register';
 import { Home } from './pages/home/home';
 import { AddProduct } from './pages/add-product/add-product';
 import { MyProducts } from './pages/my-products/my-products';
-
 import { Catalog } from './pages/catalog/catalog';
-
 import { ProductDetails } from './pages/product-details/product-details';
 import { ChatComponent } from './pages/chat/chat';
 import { MyProductDetails } from './pages/my-product-details/my-product-details';
 import { Profile } from './pages/profile/profile';
 import { Favorites } from './pages/favorites/favorites';
 import { PublicProfile } from './pages/public-profile/public-profile';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'home', component: Home },
-  { path: 'profile', component: Profile },
-  { path: 'add-product', component: AddProduct },
-  { path: 'my-products', component: MyProducts },
-  { path: 'catalog', component: Catalog },
-  { path: 'favorites', component: Favorites },
-  { path: 'product/:id', component: ProductDetails },
-  { path: 'my-product/:id', component: MyProductDetails },
-  { path: 'chat', component: ChatComponent },
-  { path: 'user/:id', component: PublicProfile },
+  { path: 'home', component: Home, canActivate: [authGuard] },
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
+  { path: 'add-product', component: AddProduct, canActivate: [authGuard] },
+  { path: 'my-products', component: MyProducts, canActivate: [authGuard] },
+  { path: 'catalog', component: Catalog, canActivate: [authGuard] },
+  { path: 'favorites', component: Favorites, canActivate: [authGuard] },
+  { path: 'product/:id', component: ProductDetails, canActivate: [authGuard] },
+  { path: 'my-product/:id', component: MyProductDetails, canActivate: [authGuard] },
+  { path: 'chat', component: ChatComponent, canActivate: [authGuard] },
+  { path: 'user/:id', component: PublicProfile, canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
